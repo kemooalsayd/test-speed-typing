d81ee0e9ed1d04e5ded47f8a4e32d4fe6e1af56b
Kareem Alsayd <106820544+kemooalsayd@users.noreply.github.com>
2022-10-13 08:10:38 +0200

    small update
    perevent paste event

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// call html elements
 let input=document.getElementById('inp')
 let word=document.getElementById('word')
 let btn=document.getElementById('btn')
 let result=document.querySelector('#last-result span')
-
+// list of words
 let list=[
     'kareem',
     'mohamed',
@@ -49,25 +50,25 @@ let id;
 let time=0
 let count=0
 let get=localStorage.getItem('high-result');
-
+// check if there result i localStorage
 if(!get){
     localStorage.setItem('high-result',0)
 }else{
     result.innerHTML=`${get}W/M`
 }
 
-
+// set html content in word field
 word.innerHTML=list[0]
-
+// set html Attribute  in input field
 input.setAttribute('maxlength',list[0].length)
-
+// handel click event
 btn.onclick=()=>{
     input.removeAttribute('disabled')
     input.focus()
     btn.remove()
     id=setInterval(timeFunc,1000)
 }
-
+// time function
 function timeFunc(){
     time+=1;
     if(time==60){
@@ -77,9 +78,11 @@ function timeFunc(){
         window.location.reload()
     }
 }
-
-
-
+// handle paste event
+input.onpaste=()=>{
+    return false
+}
+// handle keyup event
 input.onkeyup=()=>{
         if (word.innerHTML.length==input.value.length){
             if(word.innerHTML==input.value){
